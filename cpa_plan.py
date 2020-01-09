664f15a9d3adf68a398080ff131007eb4478984d
Chen <38043557+bdochen@users.noreply.github.com>
2020-01-09 22:21:36 +0800

    Update cpa_plan.py

--- cpa_plan.py
+++ cpa_plan.py
@@ -1,2 +1,16 @@
 一个月复习完一门
 2019.10.24开始复习财务第二章，计划于2019.11.24前复习完整本财管
+
+
+@echo off
+Setlocal enabledelayedexpansion
+
+Set "BeforeStr=被替换的内容"
+Set "AfterStr=新内容"
+
+For /r %%# in (*) Do (
+    Set "File=%%~nx#"
+    Ren "%%#" "!File:%BeforeStr%=%AfterStr%!"
+)
+
+Pause&Exit
